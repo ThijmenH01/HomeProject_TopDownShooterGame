5b6cbbb20c90e9a2744291e2b2dfe0ae4cee999e
Thijmen <Thijmenh01@gmail.com>
2019-10-18 13:33:08 +0200

    Ep 2

--- TopDownShooterGame_Unity/Assets/Scripts/Player.cs
+++ TopDownShooterGame_Unity/Assets/Scripts/Player.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof (PlayerController))]
+[RequireComponent(typeof (GunController))]
 public class Player : MonoBehaviour {
     public float moveSpeed = 5;
 
     private Camera viewCamera;
     private PlayerController controller;
+    private GunController gunController;
 
     private void Awake() {
         viewCamera = Camera.main;
         controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
     }
 
     void Start() {
@@ -19,10 +22,12 @@ public class Player : MonoBehaviour {
     }
 
     void Update() {
+        //Movement
         Vector3 moveInput = new Vector3( Input.GetAxisRaw( "Horizontal" ) , 0 , Input.GetAxisRaw( "Vertical" ) );
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move( moveVelocity );
 
+        //Look
         Ray ray = viewCamera.ScreenPointToRay( Input.mousePosition );
         Plane groundPlane = new Plane( Vector3.up , Vector3.zero );
         float rayDist;
@@ -31,6 +36,11 @@ public class Player : MonoBehaviour {
             Vector3 point = ray.GetPoint( rayDist );
             Debug.DrawLine( ray.origin , point , Color.red );
             controller.LookAt( point );
+        }
+
+        //Weapon
+        if(Input.GetMouseButton( 0 )) {
+            gunController.Shoot();
         }
     }
 }
